050804ab0f9a8717e1a81b7ad22f45cf60d80cb2
maojiashun <905303446@qq.com>
2017-05-16 23:06:17 +0800

    Create init_import.py

---
+++ init_import.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+import time,sys
+from sklearn import metrics  
+import pickle as pickle  
+import pprint
+import pandas as pd
+import numpy   as np
+import matplotlib.pyplot as plt  
+import  xdrlib
+import xlrd,xlwt,re
+import time,datetime
+from sqlalchemy import create_engine
+reload(sys)  
+sys.setdefaultencoding('utf8') 
+#import xgboost as xgb
+#import operator
